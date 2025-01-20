2deafd0ce699b82cbba26005c61927c1d324b944
shaharia-72 <shahariahossain72@gmail.com>
2025-01-20 17:08:44 +0600

    updatedsss

--- project_event_management/settings.py
+++ project_event_management/settings.py
@@ -61,6 +61,7 @@ CSRF_TRUSTED_ORIGINS = [
     "https://*.127.0.0.1",
     "http://127.0.0.1:3000",
     "http://localhost:3000",
+    "https://event-managment-849k.onrender.com"
 ]
 
 # Template settings
